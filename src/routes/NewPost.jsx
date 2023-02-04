abdfa65de4ff4ed10c3895ffb0f2336435eb5d1c
unknown <santifp1997@gmail.com>
2023-02-03 23:26:57 -0300

    chat

---
+++ src/routes/NewPost.jsx
@@ -0,0 +1,44 @@
+import { Link, Form, redirect } from 'react-router-dom';
+
+import classes from './NewPost.module.css';
+import Modal from '../components/Modal';
+
+function NewPost() {
+  return (
+    <Modal>
+      <Form method='post' className={classes.form}>
+        <p>
+          <label htmlFor="body">Text</label>
+          <textarea id="body" name="body" required rows={3} />
+        </p>
+        <p>
+          <label htmlFor="name">Your name</label>
+          <input type="text" id="name" name="author" required />
+        </p>
+        <p className={classes.actions}>
+          <Link to=".." type="button">
+            Cancel
+          </Link>
+          <button>Submit</button>
+        </p>
+      </Form>
+    </Modal>
+  );
+}
+
+export default NewPost;
+
+export async function action({request}) {
+  
+  const formData = await request.formData();
+  const postData = Object.fromEntries(formData); // { body: '...', author: '...' }
+  await fetch('https://pepito-e96e6-default-rtdb.firebaseio.com/posts.json', {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return redirect('/');
+}
---
+++ src/routes/Posts.jsx
@@ -0,0 +1,43 @@
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import PostsList from "../components/PostsList";
+
+function Posts() {
+
+  const [posts,setPosts] = useState([]);
+
+  useEffect(() => {
+    const timer = setInterval(async () => {
+      const response = await fetch(
+        "https://pepito-e96e6-default-rtdb.firebaseio.com/posts.json"
+      );
+      const resData = await response.json();
+      const loadedPosts = [];
+      for (const key in resData) {
+        loadedPosts.push({
+          id: key,
+          author: resData[key].author,
+          body: resData[key].body,
+        });
+      }
+      return loadedPosts ? setPosts(loadedPosts) : [];
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  console.log(posts)
+  return (
+    <>
+      <Outlet />
+      <main>
+        <PostsList posts={posts} />
+      </main>
+    </>
+  );
+}
+
+export default Posts;
